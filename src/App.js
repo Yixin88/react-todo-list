0a9537f156fab55518d1c3f5eae009398bc37621
Yixin <yixin88@hotmail.co.uk>
2023-02-27 23:19:30 +0000

    added filter for all, todo and completed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import React, { useContext, useState, useEffect } from 'react'
 function App() {
 
   const [input, setInput] = useState("");
+  const [filter, setFilter] = useState("All");
   const {task, setTask} = useContext(Tasks);
   const {numberOfTask, setNumberOfTask} = useContext(numOfTask);
 
@@ -61,7 +62,6 @@ function App() {
     }))
   }
 
-
   return (
     <div className="divBox flex flex-col items-center justify-center h-screen w-screen font-mono bg-sky-100">
 
@@ -70,37 +70,53 @@ function App() {
       <div className='flex justify-center mt-[20px]'>
         <input className='border-2 border-sky-500 rounded-l-lg px-4 py-1 max-w-2xl min-w-[300px] w-[70vw]' type="text" value={input} onChange={(e) => setInput(e.target.value)} />
         <button className='rounded-r-lg bg-sky-500 px-4 py-1 hover:bg-green-500' onClick={() => addTodo(input)}>Add</button>
+          <select onChange={(e)=>setFilter(e.target.value)}>
+            <option value="all">All</option>
+            <option value="todo">To-Do</option>
+            <option value="completed">Completed</option>
+          </select>
       </div>
 
-      <p className='mt-[20px] text-2xl'>Tasks Remaining: {numberOfTask}</p>
 
-      <ul className='flex flex-col gap-y-2 p-4 h-96 bg-teal-400 max-w-6xl min-w-[320px] w-[85vw] items-center rounded-[10px]'>
-        {task.filter((arrayItem)=> arrayItem.complete !== true).map((item) => {
-        return(
-          <li key={item.id} className='test flex items-center justify-between max-w-5xl min-w-[300px] gap-x-5 p-5 bg-teal-50 rounded w-[80vw]'>
-            <p className='truncate text-lg'>{item.todo}</p>
-            <div className='flex'>
-              {item.complete === false && <button className='rounded-l-lg bg-lime-500 px-4 py-1 hover:bg-lime-600 hover:text-white' onClick={() => completeHandler(item.id)}>Complete</button>}
-              <button className='bg-red-500 py-1 px-2.5 rounded-r-lg hover:bg-red-600 hover:text-white' onClick={() => removeToDo(item.id, item.complete)}>&times;</button>
-            </div>
-          </li>
-        )
-      })}
-      </ul>
-
-      <h2 className='mt-[20px] text-2xl'>Completed Tasks</h2>
-
-      <ul className='flex flex-col gap-y-2 p-4 h-96 bg-green-400 max-w-6xl min-w-[320px] w-[85vw] items-center rounded-[10px] mb-[20px]'>
-        {task.filter((arrayItem)=> arrayItem.complete === true).map((item) => {
-          return(
-            <li key={item.id} className='test flex items-center justify-between max-w-5xl min-w-[300px] gap-x-5 p-5 bg-teal-50 rounded w-[80vw]'>
-              <p className='truncate line-through decoration-2 text-lg'>{item.todo}</p>
-              <button className='rounded bg-red-500 py-1 px-2.5 hover:bg-red-600 hover:text-white' onClick={() => removeToDo(item.id, item.complete)}>&times;</button>
-            </li>
-          )
-        })}
-      </ul>
-    </div>
+      {filter && <div>
+        {(filter === "todo" || filter === "all") && 
+          <div className='flex flex-col items-center'>
+            <p className='mt-[20px] text-2xl'>Tasks Remaining: {numberOfTask}</p>
+            <ul className='flex flex-col gap-y-2 p-4 h-96 bg-teal-400 max-w-6xl min-w-[320px] w-[85vw] items-center rounded-[10px]'>
+              {task.filter((arrayItem)=> arrayItem.complete !== true).map((item) => {
+              return(
+                <li key={item.id} className='test flex items-center justify-between max-w-5xl min-w-[300px] gap-x-5 p-5 bg-teal-50 rounded w-[80vw]'>
+                  <p className='truncate text-lg'>{item.todo}</p>
+                  <div className='flex'>
+                    {item.complete === false && <button className='rounded-l-lg bg-lime-500 px-4 py-1 hover:bg-lime-600 hover:text-white' onClick={() => completeHandler(item.id)}>Complete</button>}
+                    <button className='bg-red-500 py-1 px-2.5 rounded-r-lg hover:bg-red-600 hover:text-white' onClick={() => removeToDo(item.id, item.complete)}>&times;</button>
+                  </div>
+                </li>
+              )
+            })}
+            </ul>
+          </div>
+        }
+  
+        
+  
+        {(filter === "completed" || filter === "all") &&  
+          <div className='flex flex-col items-center'>
+            <h2 className='mt-[20px] text-2xl'>Completed Tasks</h2>
+            <ul className='flex flex-col gap-y-2 p-4 h-96 bg-green-400 max-w-6xl min-w-[320px] w-[85vw] items-center rounded-[10px] mb-[20px]'>
+              {task.filter((arrayItem)=> arrayItem.complete === true).map((item) => {
+                return(
+                  <li key={item.id} className='test flex items-center justify-between max-w-5xl min-w-[300px] gap-x-5 p-5 bg-teal-50 rounded w-[80vw]'>
+                    <p className='truncate line-through decoration-2 text-lg'>{item.todo}</p>
+                    <button className='rounded bg-red-500 py-1 px-2.5 hover:bg-red-600 hover:text-white' onClick={() => removeToDo(item.id, item.complete)}>&times;</button>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        }
+      </div>}
+      </div>
   );
 }
 
