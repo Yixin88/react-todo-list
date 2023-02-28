8ae22483ec5d65495d2da8f1259a17584da18974
Yixin <yixin88@hotmail.co.uk>
2023-02-28 15:57:27 +0000

    actually fixed issue with counter context in local storage

--- src/context/Counter.js
+++ src/context/Counter.js
@@ -6,11 +6,10 @@ export const numOfTask = createContext();
 const getLocalStorage = () => {
     let taskCounter = localStorage.getItem("numberOfTask")
     if (taskCounter) {
-        console.log('if ran')
       return (taskCounter = parseInt(localStorage.getItem("numberOfTask")))
     } else {
-        console.log('else ran')
-      return localStorage.setItem("numberOfTask", JSON.stringify(0));
+        localStorage.setItem("numberOfTask", JSON.stringify(0))
+      return (taskCounter = parseInt(localStorage.getItem("numberOfTask")));
     }
   }
 
